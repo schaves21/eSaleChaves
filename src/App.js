aaed721e8574ade86c9456dc4ba34d05f0612e6d
schaves <schaves.ap@gmail.com>
2022-05-28 09:28:09 -0300

    Menu e-Commerce

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <h1>Bienvenidos al mejor sitio de Compras rebajadas</h1>;
+  return (
+    <>
+      <NavBar /> 
+    </>
+  );
 }
 
 export default App;
